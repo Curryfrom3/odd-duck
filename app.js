6aa2128244f4e69d566b7153547c4e53242755d3
Deon Curry <deoncvn68@gmail.comm>
2023-02-07 17:13:23 -0800

    lab12

--- app.js
+++ app.js
@@ -1,60 +1,205 @@
-// Constructor function to create an object for each product
-function Product(name, filePath) {
+'use strict';
+// GLOBALS 
+
+// Line 10-12: The code declares 3 global variables:
+
+// pictureArray: an array to store RandomImage objects
+// votingRounds: an integer representing the number of voting rounds available
+// preIndex: an array to store the index of the last 3 images displayed
+
+let pictureArray = [];
+let votingRounds = 25;
+let preIndex = [-1, -1, -1];
+
+
+// DOM WINDOWS
+
+// Line 24-32: The code declares 6 DOM window variables to access HTML elements with the same id as the value:
+
+// imgContainer: the container of all images
+// imgOne, imgTwo, imgThree: the three images displayed to the user for voting
+// resultsBtn: the button to show the results
+// canvasElem: the canvas for displaying the results chart
+
+let imgContainer = document.getElementById('img-container');
+
+let imgOne = document.getElementById('img-one');
+let imgTwo = document.getElementById('img-two');
+let imgThree = document.getElementById('img-three');
+
+
+let resultsBtn = document.getElementById('show-results-btn');
+let canvasElem = document.getElementById('myChart');
+
+// CONSTRUCTOR FUNCTION
+
+// Line 38-44: The code declares a constructor function for RandomImage objects that takes in 2 parameters: name and imgExtension. The function initializes 4 properties: name, img, votes, and views. The img property is a string that combines the name and imgExtension to create the full image path.
+
+function RandomImage(name, imgExtension = 'jpg') {
   this.name = name;
-  this.filePath = filePath;
-  this.timesShown = 0;
-  this.timesClicked = 0;
-  Product.all.push(this);
+  this.img = `img/${name}.${imgExtension}`;
+  this.votes = 0;
+  this.views = 0;
+
 }
 
-// Property attached to the constructor function to keep track of all the products
-Product.all = [];
+// HELPER FUNCTIONS / UTILITIES 
 
-// Variable to hold the number of rounds
-var rounds = 25;
+// Line 50-51: The code declares a randomIndex() function that returns a random integer from 0 to the length of the pictureArray minus 1.
 
-// Function to generate three unique products and display them
-function displayProducts() {
-  var products = getThreeUniqueProducts();
-  products.forEach(function(product) {
-    product.timesShown++;
-    // Display the product in the HTML
-  });
+function randomIndex() {
+  return Math.floor(Math.random() * pictureArray.length);
 }
 
-// Function to get three unique products
-function getThreeUniqueProducts() {
-  var products = [];
-  while (products.length < 3) {
-    var randomIndex = Math.floor(Math.random() * Product.all.length);
-    var product = Product.all[randomIndex];
-    if (!products.includes(product)) {
-      products.push(product);
+// Line 56-92: The code declares a renderImg() function to display three random images to the user. The function first generates 3 random indices for each image using the randomIndex() function. Then, the function uses a while loop to validate that the 3 indices are unique and not the same as the previous 3 indices stored in the preIndex array. The function sets the source, title, and alt attributes of each image to the corresponding properties of the RandomImage object at the corresponding index in the pictureArray. It also increments the views property of each RandomImage object by 1.
+
+function renderImg() {
+  let imgOneIndex = randomIndex();
+  let imgTwoIndex = randomIndex();
+  let imgThreeIndex = randomIndex();
+
+  // Validation to make sure numbers are unique
+  while (preIndex.includes(imgOneIndex) || imgOneIndex === imgTwoIndex || imgOneIndex === imgThreeIndex) {
+    imgOneIndex = randomIndex();
+  }
+
+  while (preIndex.includes(imgTwoIndex) || imgOneIndex === imgTwoIndex || imgTwoIndex === imgThreeIndex) {
+    imgTwoIndex = randomIndex();
+  }
+
+  while (preIndex.includes(imgThreeIndex) || imgThreeIndex === imgTwoIndex || imgOneIndex === imgThreeIndex) {
+    imgThreeIndex = randomIndex();
+  }
+
+  preIndex[0] = imgOneIndex;
+  preIndex[1] = imgTwoIndex;
+  preIndex[2] = imgThreeIndex;
+  console.log(preIndex);
+
+  imgOne.src = pictureArray[imgOneIndex].img;
+  imgTwo.src = pictureArray[imgTwoIndex].img;
+  imgThree.src = pictureArray[imgThreeIndex].img;
+  imgOne.title = pictureArray[imgOneIndex].name;
+  imgOne.alt = `this is an image of ${pictureArray[imgOneIndex].name}`;
+  imgTwo.title = pictureArray[imgTwoIndex].name;
+  imgTwo.alt = `this is an image of ${pictureArray[imgTwoIndex].name}`;
+  imgThree.title = pictureArray[imgThreeIndex].name;
+  imgThree.alt = `this is an image of ${pictureArray[imgThreeIndex].name}`;
+
+  pictureArray[imgOneIndex].views++;
+  pictureArray[imgTwoIndex].views++;
+  pictureArray[imgThreeIndex].views++;
+}
+
+// Line 96-134: The code declares a renderChart() function to display the results of the voting. The function first declares 3 arrays: pictureName, pictureVotes, and pictureViews. It uses a for loop to fill each array with the corresponding properties from the RandomImage objects stored in the pictureArray. The function then creates a chart object with type 'bar', data, and options properties and uses the Chart library to create a chart with this object.
+
+function renderChart() {
+  let pictureName = [];
+  let pictureVotes = [];
+  let pictureViews = [];
+
+  for (let i = 0; i < pictureArray.length; i++) {
+    pictureName.push(pictureArray[i].name);
+    pictureVotes.push(pictureArray[i].votes);
+    pictureViews.push(pictureArray[i].views);
+  }
+
+  let chartObj = {
+    type: 'bar',
+    data: {
+      labels: pictureName,
+      datasets: [{
+        label: '# of Votes',
+        data: pictureVotes,
+        borderWidth: 1
+
+      },
+      {
+        label: '# of Views',
+        data: pictureViews,
+        borderWidth: 1
+
+      }]
+    },
+    options: {indexAxis: 'y',
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  };
+
+  new Chart(canvasElem, chartObj);
+}
+
+// EVENT HANDLERS
+
+// Line 140-160: The code declares a handleClick() function to handle the click event on the images. The function first retrieves the title of the clicked image and uses a for loop to find the corresponding RandomImage object in the pictureArray. It increments the votes property of the RandomImage object by 1 and decrements the votingRounds by 1. The function calls the renderImg() function to display the next set of images and checks if the votingRounds equals 0. If so, it removes the click event handler on the imgContainer. The function also stringifies the pictureArray and stores it in local storage under the key 'myPictures'.
+
+function handleClick(event) {
+
+  let imgClicked = event.target.title;
+
+  for (let i = 0; i < pictureArray.length; i++) {
+    if (imgClicked === pictureArray[i].name) {
+      pictureArray[i].votes++;
     }
   }
-  return products;
-}
 
-// Event listener to listen for the product selection
-document.getElementById("products").addEventListener("click", function(event) {
-  if (event.target.tagName === "IMG") {
-    var productName = event.target.alt;
-    var product = Product.all.find(function(product) {
-      return product.name === productName;
-    });
-    product.timesClicked++;
-    rounds--;
-    if (rounds > 0) {
-      displayProducts();
-    } else {
-      // Remove the event listener
-      document.getElementById("products").removeEventListener("click");
-      // Show the results button
-    }
+  votingRounds--;
+  renderImg();
+  if (votingRounds === 0) {
+    imgContainer.removeEventListener('click', handleClick);
   }
-});
 
-// Function to show the results
-function showResults() {
-  // Loop through all the products and display the results
+  let stringifiedPictures = JSON.stringify(pictureArray);
+  localStorage.setItem('myPictures', stringifiedPictures);
+  console.log('Stringified data', stringifiedPictures);
+
 }
+
+// Line 164-168: The code declares a handleResults() function to handle the click event on the results button. The function checks if the votingRounds equals 0 and calls the renderChart() function to display the results if so.
+
+function handleResults() {
+  if (votingRounds === 0) {
+    renderChart();
+  }
+}
+// EXECUTABLE CODE 
+
+// Line 173-end: The code uses local storage to retrieve the stored pictureArray from a previous session, if any, and assigns it to a variable parsedData. If parsedData is not null, the code assigns it to the pictureArray. If parsedData is null, the code creates a new array of RandomImage objects and assigns it to the pictureArray. The code then adds the click event handler on the imgContainer to call the handleClick() function and the click event handler on the resultsBtn to call the handleResults() function. 
+
+let storedPictures = localStorage.getItem('myPictures');
+let parsedData = JSON.parse(storedPictures);
+
+if (storedPictures) {
+  pictureArray = parsedData;
+} else {
+  let bag = new RandomImage('bag');
+  let banana = new RandomImage('banana');
+  let bathroom = new RandomImage('bathroom');
+  let boots = new RandomImage('boots');
+  let breakfast = new RandomImage('breakfast');
+  let bubblegum = new RandomImage('bubblegum');
+  let chair = new RandomImage('chair');
+  let cthulhu = new RandomImage('cthulhu');
+  let dogDuck = new RandomImage('dog-duck');
+  let dragon = new RandomImage('dragon');
+  let pen = new RandomImage('pen');
+  let petSweep = new RandomImage('pet-sweep');
+  let scissors = new RandomImage('scissors');
+  let shark = new RandomImage('shark');
+  let sweep = new RandomImage('sweep', 'png');
+  let tauntaun = new RandomImage('tauntaun');
+  let unicorn = new RandomImage('unicorn');
+  let waterCan = new RandomImage('water-can');
+  let wineGlass = new RandomImage('wine-glass');
+
+  pictureArray.push(bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, sweep, tauntaun, unicorn, waterCan, wineGlass);
+}
+
+renderImg();
+
+imgContainer.addEventListener('click', handleClick);
+resultsBtn.addEventListener('click', handleResults);
